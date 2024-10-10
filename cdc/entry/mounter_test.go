7122baed0900bfdf1412631bf05ba2983f715eab
xiabee <yunjie.xiao@pingcap.com.cn>
2024-10-10 12:27:04 +0800

    security test for release-8.4

--- cdc/entry/mounter_test.go
+++ cdc/entry/mounter_test.go
@@ -27,9 +27,9 @@ import (
 	"github.com/pingcap/tidb/pkg/executor"
 	tidbkv "github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/meta/autoid"
+	timodel "github.com/pingcap/tidb/pkg/meta/model"
 	"github.com/pingcap/tidb/pkg/parser"
 	"github.com/pingcap/tidb/pkg/parser/ast"
-	timodel "github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/session"
 	"github.com/pingcap/tidb/pkg/store/mockstore"
@@ -269,7 +269,7 @@ func testMounterDisableOldValue(t *testing.T, tc struct {
 	ticonfig.UpdateGlobal(func(conf *ticonfig.Config) {
 		// we can update the tidb config here
 	})
-	session.SetSchemaLease(0)
+	session.SetSchemaLease(time.Second)
 	session.DisableStats4Test()
 	domain, err := session.BootstrapSession(store)
 	require.Nil(t, err)
@@ -1682,4 +1682,13 @@ func TestFormatColVal(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, float32(0), value)
 	require.NotZero(t, warn)
+
+	vector, _ := types.ParseVectorFloat32("[1,2,3,4,5]")
+	ftTypeVector := types.NewFieldType(mysql.TypeTiDBVectorFloat32)
+	col = &timodel.ColumnInfo{FieldType: *ftTypeVector}
+	datum.SetVectorFloat32(vector)
+	value, _, warn, err = formatColVal(datum, col)
+	require.NoError(t, err)
+	require.Equal(t, vector, value)
+	require.Zero(t, warn)
 }
